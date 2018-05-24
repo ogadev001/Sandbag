850f5ab501e35e75fe189a6129a568a65f07f1fa
Ogasys <ogadev001@gmail.com>
2018-05-24 15:37:50 +0900

    komitto

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Larxavel
                 </div>
                 <div class="title m-b-md">
                     sandbag
