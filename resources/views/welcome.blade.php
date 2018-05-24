44c7acbced7c081ab15975111dd29caee7c2493c
Ogasys <ogadev001@gmail.com>
2018-05-24 16:03:36 +0900

    タイトル修正

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Larxavel
+                    Laravel
                 </div>
                 <div class="title m-b-md">
                     sandbag
